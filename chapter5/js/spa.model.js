44f7d31013065695acfdce30979474d1c60b65fe
lseyesl <lseyesl@163.com>
2016-05-04 01:20:13 -0700

    chapter5

---
+++ chapter5/js/spa.model.js
@@ -0,0 +1,87 @@
+/*
+	span.model.js
+*/
+
+/*
+	jslint
+	browser:true,coninue:true,devel:true,
+	indent:2,maxerr:50,newcap:true,nomen:true,
+	plusplus:true,regexp:true,sloppy:true,vars:false,
+	white:true
+*/
+
+/*global $,spa*/
+
+spa.model = (function(){
+
+	'use strict';
+	var 
+	configMap = {anon_id:'a0'},
+	stateMap = {
+		anon_user :null,
+		people_cid_map :{},
+		people_db : TAFFY()
+	},
+	isFakeData = true
+	personProto,makePerson,people,initModule;
+
+	personProto = {
+		get_is_user : function(){
+			return this.cid === stateMap.user.cid;	
+		},
+		get_is_anon : function(){
+			return this.cid === stateMap.anon_user.cid;	
+		}
+	},
+
+	makePerson = function(person_map){
+		var person,
+		cid = person_map.cid,
+		css_map = person_map.css_map,
+		id = person_map.id,
+		name = person_map.name;
+
+		if(cid === undefined || !name){
+			throw 'client id and name required';	
+		}
+
+		person = Object.create(personProto);
+		person.cid = cid;
+		person.name = name;
+		person.css_map = css_map;
+
+		if(id){
+			person.id = id;	
+		}
+
+		sateMap.people_cid_map[cid]= person;
+		stateMap.people_db.insert(person);
+		return person;
+	};
+
+	people = {
+		get_db:function(){
+			return stateMap.people_db;	
+		},
+		get_cid_map:function(){
+			return stateMap.people_cid_map;	
+		}
+	};
+
+	initModule = function(){
+		
+		var i,people_list,person_map;
+
+		stateMap.anon_user = makePerson({
+			cid : configMap.anon_id,
+			id : configMap.anon_id,
+			name : 'anonymous'
+		});
+
+		stateMap.user = stateMap.anon_user;
+
+		if(isFakeData){
+			
+		}
+	}
+}());
