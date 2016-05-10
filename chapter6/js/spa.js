642430447061ae85c8b90549c0e0485a94cb61bc
lseyesl <lseyesl@163.com>
2016-05-10 02:40:34 -0700

    chapter6

---
+++ chapter6/js/spa.js
@@ -0,0 +1,20 @@
+/*
+spa.js
+*/
+
+/*jslint   browser:true, continue:true,
+devel:true, indent:2, maxerr:50,
+newcap:true, nomen:true, plusplus:true,
+regexp:true, sloppy:true, vars:false,
+white:true
+*/
+
+var spa = (function(){
+	'use strict';
+	var initModule = function($container){
+		spa.model.initModule();
+		spa.shell.initModule($container);
+	};
+	
+	return {initModule:initModule};
+}());
