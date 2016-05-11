d6951d9f9cbefd8dfc72fd01adfa7d0e708ea4e7
lseyesl <lseyesl@163.com>
2016-05-11 02:13:03 -0700

    fake add peoplelist

--- chapter6/js/spa.fake.js
+++ chapter6/js/spa.fake.js
@@ -73,6 +73,7 @@ spa.fake = (function(){
 		var 
 		on_sio,
 		emit_sio,
+		emit_mock_msg,
 		send_listchange,
 		listchange_idto,
 		callback_map={};
@@ -93,13 +94,57 @@ spa.fake = (function(){
 					peopleList.push(person_map);
 					callback_map.userupdate([person_map]);
 				},3000);	
-			}	
+			}
+
+			if(msg_type === 'updatechat' && callback_map.updatechat){
+				setTimeout(function(){
+					var user = spa.model.people.get_user();
+					callback_map.updatechat([
+						{
+							dest_id:user.id,
+							dest_name:user.name,
+							sender_id:data.dest_id,
+							msg_text:'Thanks for the note, '+user.name
+						}
+					]);
+				},2000);	
+			}
+
+			if(msg_type === 'leavechat'){
+				delete callback_map.listchange;
+				delete callback_map.updatechat;
+
+				if(listchange_idto){
+					clearTimeout(listchange_idto);
+					listchange_idto = undefined;
+				}
+				send_listchange();
+			}
+		};
+
+		emit_mock_msg = function(){
+			setTimeout(function(){
+				var user = spa.model.people.get_user();
+				if(callback_map.updatechat){
+					callback_map.updatechat([
+						{
+							dest_id:user.id,
+							dest_name:user.name,
+							sender_id:'id_04',
+							msg_text:'Hi there '+ user.name + '! wilma here.'
+						}
+					]);	
+				}else{
+					emit_mock_msg();	
+				}
+			},8000);	
 		};
 
 		send_listchange = function(){
 			listchange_idto = setTimeout(function(){
 				if(callback_map.listchange){
 					callback_map.listchange([peopleList]);
+					emit_mock_msg();
 					listchange_idto = undefined;
 				}else{
 					send_listchange();	
@@ -111,6 +156,7 @@ spa.fake = (function(){
 	}());
 
 	return {
-		mockSio:mockSio
+		mockSio:mockSio,
+		people:peopleList
 		};
 }());
