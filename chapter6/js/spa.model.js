6f30b887ca9f0f11e4dd185824ffd3839a9dd618
lseyesl <lseyesl@163.com>
2016-05-25 22:29:05 -0700

    client done

--- chapter6/js/spa.model.js
+++ chapter6/js/spa.model.js
@@ -25,7 +25,7 @@ spa.model = (function(){
 		user:null,
 		is_connected:false
 	},
-	isFakeData = true,
+	isFakeData = false,
 	personProto,makeCid,clearPeopleDb,completeLogin,
 	makePerson,removePerson,people,chat,initModule;
 
--- chapter6/js/spa.shell.js
+++ chapter6/js/spa.shell.js
@@ -248,7 +248,13 @@ spa.shell = (function(){
 			people_model:spa.model.people
 		});
 		spa.chat.initModule(jqueryMap.$container);
+		
+		spa.avtr.configModule({
+			chat_model:spa.model.chat,
+			people_model:spa.model.people
+		});
 
+		spa.avtr.initModule(jqueryMap.$nav);
 		$(window)
 		.bind('resize',onResize)
 		.bind('hashchange',onHashchange)
