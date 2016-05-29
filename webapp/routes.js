36c332b4d62db7cb580a1ca8ec2213e9aeaee86b
lseyesl <lseyesl@163.com>
2016-05-29 00:36:03 -0700

    socketio

---
+++ webapp/routes.js
@@ -0,0 +1,50 @@
+/*
+	router.js module to provide routing
+*/
+
+'use strict'
+
+var configRoutes;
+
+configRotes = function(app,server){
+	app.get('/',function(request,response){
+		response.redirect('/spa.html');
+	});
+
+	app.all('/:obj_type/*?',function(request,response,next){
+		response.contentTpe('json');
+		next();
+	});
+	
+	app.get('/:obj_type/list',function(request,response){
+		response.send({title:request.params.obj_type+ ' list'});
+	});
+
+	app.get('/:obj_type/create',function(request,response){
+		response.send({title:request.params.obj_type+' create'});	
+	});
+
+	app.get('/:obj_type/read/:id([0-9]+)',function(request,response){
+		response.send({
+			title:request.params.obj_type
+			+' with id '+ request.params.id+ ' found'
+		});
+	});
+
+	app.get('/:obj_type/update/:id([0-9]+)',function(request,response){
+		response.send({
+			title:request.params.obj_type
+			+' with id '+ request.params.id+ ' updated'
+		});
+	});
+
+	app.get('/:obj_type/delete/:id([0-9]+)',function(request,response){
+		response.send({
+			title:request.params.obj_type
+			+' with id '+request.params.id + ' delete'
+		});
+	});
+};
+
+module.exports = {configRoutes:configRoutes};
+
